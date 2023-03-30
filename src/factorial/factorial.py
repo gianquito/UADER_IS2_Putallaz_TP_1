a18f53e8def8b58826a2910e971a17497ccc69de
BuildTools <gianputallaz@gmail.com>
2023-03-30 01:06:36 -0300

    agregar comentarios

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -21,18 +21,21 @@ def factorial(num):
             num -= 1
         return fact 
 
-#Controla que se pase un numero como argumento, sino lo solicita
 min = 1
 max = 60
+#Controla que se pase un numero como argumento, sino lo solicita
 if len(sys.argv) < 2:
    print("Debe informar un número!")
    num = (input('Ingrese un rango (ej: 4-8) ')).split('-')
 else:   
     num= sys.argv[1].split('-')
 
-if num[0]:
+
+if num[0]: #si tiene rango inferior lo asigna como minimo
     min = int(num[0])
-if num[1]:
+if num[1]: #si tiene rango superior lo asigna como maximo
     max = int(num[1])
+
+#calcula el factorial de los numeros en el rango dado
 for i in range(min,max + 1):
     print("Factorial ",i,"! es ", factorial(i)) 
