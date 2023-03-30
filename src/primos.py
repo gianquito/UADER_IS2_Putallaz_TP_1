ff9c213c0129779663746f0679a85b330f99ffec
BuildTools <gianputallaz@gmail.com>
2023-03-29 23:57:21 -0300

    agrego comentarios

--- src/primos.py
+++ src/primos.py
@@ -1,7 +1,8 @@
+#Le pide al usuario hasta que numero se buscan primos
 max = int(input("Find primes up to what number? : "))
 primeList = []
 #for loop for checking each number
-for x in range(2, max + 1):
+for x in range(2, max + 1): #va numero a numero (hasta el maximo) revisando si es primo
 	isPrime = True
 	index = 0
 	root = int(x ** 0.5) + 1
@@ -12,12 +13,14 @@ for x in range(2, max + 1):
 		index += 1
 	if isPrime:
 		primeList.append(x)
-print(primeList)
+print(primeList) # se muestra la lista de primos
 #-------------------------------------------------------------
 # prime number calculator: find the first n primes
+#Le pide al usuario cuantos numeros primos calcular
 count = int(input("Find how many primes?: "))
 primeList = []
 x = 2
+#Calcula los numeros primos
 while len(primeList) < count:
 	isPrime = True
 	index = 0
