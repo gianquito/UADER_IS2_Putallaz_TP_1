878189bcd40ef9b31e7868774275b652528fe5a1
BuildTools <gianputallaz@gmail.com>
2023-04-11 10:14:25 -0300

    cadenas try/except

--- src/chatGPT/main.py
+++ src/chatGPT/main.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
 import openai
 #……. [ mas Código de inicialización aqui ] …………
-openai.api_key = "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"
+openai.api_key = ""
 TOP_P=1
 FREQ_PENALTY=0
 PRES_PENALTY=0
@@ -11,21 +10,35 @@ TEMPERATURE=0.75
 NMAX=1
 MODEL_ENGINE = "text-davinci-003"
 
-userText = input('You: ')
-if(userText != ''):
-    userText = 'You: ' + userText + '\nchatGPT:'
-    # Set up the model and prompt
-    completion = openai.Completion.create(
-                    engine=MODEL_ENGINE,
-                    prompt=userText,
-                    max_tokens=MAX_TOKENS,
-                    n=NMAX,
-                    top_p=TOP_P,
-                    frequency_penalty=FREQ_PENALTY,
-                    presence_penalty=PRES_PENALTY,
-                    temperature=TEMPERATURE,
-                    stop=STOP)
+try:
+    userText = input('You: ')
+    if(userText == ''):
+        raise Exception("Cadena vacia")
+    try:
+        userText = 'You: ' + userText + '\nchatGPT:'
+        try:
+            # Set up the model and prompt
+            completion = openai.Completion.create(
+                            engine=MODEL_ENGINE,
+                            prompt=userText,
+                            max_tokens=MAX_TOKENS,
+                            n=NMAX,
+                            top_p=TOP_P,
+                            frequency_penalty=FREQ_PENALTY,
+                            presence_penalty=PRES_PENALTY,
+                            temperature=TEMPERATURE,
+                            stop=STOP)
 
-    print('chatGPT:' + completion.choices[0].text)
-else:
-    print('La consulta está vacia')
+            print('chatGPT:' + completion.choices[0].text)
+        except:
+            print("Error llamando a la API")
+    except:
+        print("Error en el tratamiento de la cadena")
+except:
+    print("Error en la consulta del usuario")
+# if(userText != ''):
+    
+   
+
+# else:
+#     print('La consulta está vacia')
